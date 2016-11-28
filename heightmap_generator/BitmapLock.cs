6d26f05f214f3ccd30e84ce81862a9de08392994
coder0xff <coder0xff@gmail.com>
2016-11-27 19:42:38 -0500

    init

---
+++ heightmap_generator/BitmapLock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace heightmap_generator
+{
+    class BitmapLock : IDisposable
+    {
+        public readonly IntPtr Scan0;
+        public readonly int Stride;
+
+        private readonly Bitmap _bmp;
+        private readonly BitmapData _data;
+
+        public BitmapLock(Bitmap bmp, BitmapData data)
+        {
+            _bmp = bmp;
+            _data = data;
+            _bmp.LockBits(new Rectangle(0, 0, _bmp.Width, _bmp.Height), ImageLockMode.ReadWrite, _data.PixelFormat, _data);
+            Scan0 = _data.Scan0;
+            Stride = _data.Stride;
+        }
+
+        public void Dispose()
+        {
+            _bmp.UnlockBits(_data);
+        }
+    }
+
+    static class BitmapExtensions
+    {
+        public static BitmapLock Lock(this Bitmap bmp, BitmapData data)
+        {
+            return new BitmapLock(bmp, data);
+        }
+
+    }
+}
